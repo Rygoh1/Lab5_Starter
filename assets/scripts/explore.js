90f71b583151f72aa599bbe4544b2dab4961c234
Ryan Goh <rygoh@ucsd.edu>
2023-05-07 21:57:56 -0700

    finished explore and expose

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,54 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // load voices
+  const voicesSelect = document.querySelector('#voice-select');
+  let voices = [];
+
+  function loadVoices() {
+    voices = speechSynthesis.getVoices();
+    voicesSelect.innerHTML = '';
+    voices.forEach((voice, i) => {
+      const option = document.createElement('option');
+      option.value = i;
+      option.textContent = `${voice.name} (${voice.lang})`;
+      voicesSelect.appendChild(option);
+    });
+  }
+  loadVoices();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = loadVoices;
+  }
+
+  // button click event
+  const button = document.querySelector('button');
+  const face = document.querySelector('img');
+  const text = document.querySelector('#text-to-speak');
+  let talking = false;
+  let synth = null;
+
+  button.addEventListener('click', () => {
+    if (!talking) {
+      // if not talking, start talking
+      const selectedVoiceIndex = voicesSelect.value;
+      const selectedVoice = voices[selectedVoiceIndex];
+      if (!selectedVoice) return;
+      synth = new SpeechSynthesisUtterance(text.value);
+      synth.voice = selectedVoice;
+      synth.onstart = () => {
+        talking = true;
+        face.src = 'assets/images/smiling-open.png';
+      };
+      synth.onend = () => {
+        talking = false;
+        face.src = 'assets/images/smiling.png';
+      };
+      speechSynthesis.speak(synth);
+    } else {
+      // if currently talking, cancel and set face to smiling
+      speechSynthesis.cancel();
+      talking = false;
+      face.src = 'assets/images/smiling.png';
+    }
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,47 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const expose = document.getElementById("expose");
+  const hornSelect = document.getElementById("horn-select");
+  const volumeSlider = document.getElementById("volume");
+  const volumeIcon = document.querySelector("#volume-controls img");
+  const playButton = expose.querySelector("button");
+  const audio = expose.querySelector("audio");
+  const jsConfetti = new JSConfetti()
+
+  // Load the correct image and audio file when a horn is selected
+  hornSelect.addEventListener("change", () => {
+    const hornName = hornSelect.value;
+    const hornImage = expose.querySelector("img");
+    const audioSource = `./assets/audio/${hornName}.mp3`;
+    hornImage.src = `./assets/images/${hornName}.svg`;
+    audio.src = audioSource;
+  });
+
+  // Update the volume icon and the audio volume when the slider is moved
+  volumeSlider.addEventListener("input", () => {
+    const volume = volumeSlider.value;
+    console.log(volume)
+    audio.volume = volume / 100;
+    if (volume == 0) {
+      volumeIcon.src = "./assets/icons/volume-level-0.svg";
+    } else if (volume < 33) {
+      volumeIcon.src = "./assets/icons/volume-level-1.svg";
+    } else if (volume < 67) {
+      volumeIcon.src = "./assets/icons/volume-level-2.svg";
+    } else {
+      volumeIcon.src = "./assets/icons/volume-level-3.svg";
+    }
+  });
+
+  // Play the audio and shoot confetti (if Party Horn is selected) when the button is clicked
+  playButton.addEventListener("click", () => {
+    audio.currentTime = 0;
+    audio.play();
+    if (hornSelect.value == "party-horn") {
+      jsConfetti.addConfetti()
+    }
+  });
 }
+
+init();
